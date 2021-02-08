2d3e48c72d85c04cf9c8397ad8036fa75ba4d510
himanshu999 <himanshunathani999@gmail.com>
2021-02-08 13:39:17 +0000

    Update handler.js

--- handler.js
+++ handler.js
@@ -5,19 +5,19 @@ const stripe = Stripe('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 module.exports.getStripeEphemeralKeys = async event => {
   
-  //const api_version = data.api_version;
-  const customer_id = event.multiValueQueryStringParameters.customer_id;
+  const api_version = '2020-08-27';
+  const customer_id = event.multiValueQueryStringParameters.stripe_account_id;
   //const customer_id = data.customer_id;
-  /*const key = await stripe.ephemeralKeys.create(
+  const key = await stripe.ephemeralKeys.create(
         {customer: customer_id},
         {apiVersion: api_version}
-    );*/
+    );
   
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: customer_id,
+        message: key,
         input: event,
       },
       null,
