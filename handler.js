915ace9c889a5014355a6e1bb8f302732d386a03
himanshu999 <himanshunathani999@gmail.com>
2021-02-08 13:26:27 +0000

    Update handler.js

--- handler.js
+++ handler.js
@@ -1,11 +1,23 @@
 'use strict';
 
-module.exports.hello = async event => {
+const Stripe = require('stripe');
+const stripe = Stripe('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+
+module.exports.getStripeEphemeralKeys = async event => {
+  
+  //const api_version = data.api_version;
+  const customer_id = event.multiValueQueryStringParameters.customer_id;
+  //const customer_id = data.customer_id;
+  /*const key = await stripe.ephemeralKeys.create(
+        {customer: customer_id},
+        {apiVersion: api_version}
+    );*/
+  
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: customer_id,
         input: event,
       },
       null,
